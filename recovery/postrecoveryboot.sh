8ad797aa46ac66a061f6ad7b3478c99654b605c3
David Ferguson <ferguson.david@gmail.com>
2012-08-16 17:44:18 -0400

    msm8660: enable mounting of both sdcards in recovery USB mass storage
    Change-Id: I5ef1bc80cb20bdf1ac04494e95d4a5c313e3810f

--- recovery/postrecoveryboot.sh
+++ recovery/postrecoveryboot.sh
@@ -8,3 +8,9 @@
 
 sleep 3
 echo 3 > /dev/block/mmcblk0p12
+
+# Enable USB mass storage
+echo 0 > /sys/class/android_usb/android0/enable
+echo adb,mass_storage > /sys/class/android_usb/android0/functions
+echo 1 > /sys/class/android_usb/android0/enable
+
