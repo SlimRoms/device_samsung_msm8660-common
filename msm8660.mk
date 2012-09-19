073a07152d6f0e9438b7689a0ec9d36fb4a96845
Steve Kondik <shade@chemlab.org>
2012-09-19 14:50:02 -0700

    msm8660: Not all child devices have a proximity sensor
     * Move to device-specific configs, P5 doesn't have one.
     * Fixes CTS failure
    
    Change-Id: I70d155b41267f5707b06f329a08be32c2661ad2d

--- msm8660.mk
+++ msm8660.mk
@@ -21,7 +21,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
