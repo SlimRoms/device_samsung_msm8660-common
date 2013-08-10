a3c8e52a6adeaf526d36bd7855c24cd083d71eaa
Steve Kondik <shade@chemlab.org>
2013-08-10 04:22:14 -0700

    msm8660: Basic SELinux policy
     * First cut at this, just a copy from the reference.
    
    Change-Id: Ic08dd22729fc7a4bb64e78f02da40023a3d85717

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -87,3 +87,36 @@ TARGET_DISPLAY_INSECURE_MM_HEAP := true
 # Use retire fence from MDP driver
 TARGET_DISPLAY_USE_RETIRE_FENCE := true
 
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+    device/samsung/msm8660-common/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    property_contexts \
+    te_macros \
+    bridge.te \
+    camera.te \
+    conn_init.te \
+    device.te \
+    dhcp.te \
+    domain.te \
+    drmserver.te \
+    file.te \
+    kickstart.te \
+    init.te \
+    mediaserver.te \
+    mpdecision.te \
+    netmgrd.te \
+    property.te \
+    qmux.te \
+    rild.te \
+    rmt.te \
+    sensors.te \
+    surfaceflinger.te \
+    system.te \
+    tee.te \
+    thermald.te \
+    ueventd.te \
+    wpa_supplicant.te
+
