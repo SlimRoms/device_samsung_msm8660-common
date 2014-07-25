763d4fb0a09ac3faba55a91b61c599e7fa66b13f
Ethan Chen <intervigil@gmail.com>
2014-07-25 16:23:23 -0700

    msm8660-common: Cleanup Samsung device inheritance
    * Move media codecs into platform trees
    * Move postrecoveryboot script to qcom-common
    * Clean up BoardConfig for qcom-common changes
    * Add Qualcomm packages to msm8660.mk
    * Remove init.qcom.class_core and init.qcom.class_main
    * Remove init.qcom.early_boot
    * Move init.qcom.efs.sync.sh to msm8660-common
    * Move init.qcom.syspart_fixup.sh to msm8660-common
    * Remove init.qcom.sh
    * Remove init.qcom.post_boot.sh
    * Remove init.qcom.usb.sh
    * Remove duplicate idc files
    * Remove duplicate keychar files
    * Remove duplicate keylayout files
    * Use nested cm.dependencies
    * Move common platform system to msm8660-common
    * Start thermald on boot complete
    * Mount firmware partition in fstab
    * Move QCOM_BSP to platform repos
    * Move common overlay options to qcom-common
    * Move lpm.rc to msm8660-common
    * Remove unused services
    
    Change-Id: Ie95634a13205f0c5ca93cdc82f1f265e432161c5

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,5 +1,4 @@
 LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
@@ -10,19 +9,10 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.rc
+LOCAL_MODULE       := init.qcom.efs.sync.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := ueventd.qcom.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_SRC_FILES    := etc/init.qcom.efs.sync.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -39,3 +29,34 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.modem_links.sh
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.syspart_fixup.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.syspart_fixup.sh
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := lpm.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/lpm.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.qcom.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
