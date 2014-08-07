341f2cc1da8dbf5f302791bcb20db1e25e041557
bryan2894 <bryan2894@gmail.com>
2014-08-07 00:16:13 -0400

    msm8660: set dyn composition
    Change-Id: I24c1bc61e4da178a469d251055ef48b7b5c7fe7c

--- msm8660.mk
+++ msm8660.mk
@@ -46,6 +46,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=/system/lib/libqc-opt.so
 
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.composition.type=dyn \
     debug.mdpcomp.maxlayer=3 \
     debug.hwc.dynThreshold=1.9 \
     ro.opengles.version=131072
