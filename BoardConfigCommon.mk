69be8516541cccdc34a50d3664561c46d8aa57d9
Dimitris Mantzouranis <d3xter93@gmail.com>
2014-11-23 19:42:28 +0100

    msm8660-common: Use common QCOM SELinux policy
    Conflicts:
    	BoardConfigCommon.mk
    	sepolicy/bluetooth.te
    	sepolicy/device.te
    	sepolicy/file.te
    	sepolicy/file_contexts
    	sepolicy/init.te
    	sepolicy/init_shell.te
    	sepolicy/mediaserver.te
    	sepolicy/rild.te
    	sepolicy/surfaceflinger.te
    	sepolicy/ueventd.te
    	sepolicy/wpa.te
    
    Change-Id: I66a4ca460a2575b82746847e69cf3647218aefe4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -81,40 +81,7 @@ TARGET_USES_QCOM_BSP := true
 TARGET_RECOVERY_FSTAB := device/samsung/msm8660-common/rootdir/etc/fstab.qcom
 
 # SELinux
-
 include device/qcom/sepolicy/sepolicy.mk
-
-BOARD_SEPOLICY_DIRS += device/samsung/msm8660-common/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-    bluetooth.te \
-    bootanim.te \
-    device.te \
-    file.te \
-    file_contexts \
-    genfs_contexts \
-    init_shell.te \
-    kernel.te \
-    keypad_dev.te \
-    macloader.te \
-    mediaserver.te \
-    mm-qcamerad.te \
-    mpdecision.te \
-    netmgrd.te \
-    orientationd.te \
-    platform_app.te \
-    qmuxd.te \
-    rild.te \
-    rmt_storage.te \
-    surfaceflinger.te \
-    sysinit.te \
-    system_app.te \
-    system_server.te \
-    thermal-engine.te \
-    thermald.te \
-    ueventd.te \
-    vold.te \
-    wpa.te
 
 # Wifi related defines
 BOARD_HAVE_SAMSUNG_WIFI := true
