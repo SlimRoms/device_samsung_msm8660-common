cc24823b7d3445709a23cc686e8f0dc998978e4b
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2014-12-05 18:52:20 -0500

    SamsungMSM8660RIL: Remove device specific RIL for now
    Change-Id: I4714c52e1aefc38b767ead1f346528cc019b1ff6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -99,7 +99,7 @@ TARGET_RECOVERY_FSTAB := device/samsung/msm8660-common/rootdir/etc/fstab.qcom
 TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/msm8660-common
 
 # RIL
-BOARD_RIL_CLASS := ../../../device/samsung/msm8660-common/ril
+#BOARD_RIL_CLASS := ../../../device/samsung/msm8660-common/ril
 
 # SELinux
 
--- msm8660.mk
+++ msm8660.mk
@@ -66,7 +66,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # RIL Class
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.ril_class=SamsungMSM8660RIL
+    ro.telephony.ril_class=SamsungQualcommRIL
 
 # Ramdisk
 PRODUCT_PACKAGES += \
