aaa9693bac3d574e97c70a869906f941e6c02ced
bryan2894 <bryan2894@gmail.com>
2014-12-30 03:46:34 -0500

    msm8660: remove BOARD_VOLD_MAX_PARTITIONS
    * The default is set to 32 in vold
    
    Change-Id: Ie9e2d841e5b8afdd850f5e9c12e3e4b6d8723bd1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -131,5 +131,4 @@ WIFI_DRIVER_MODULE_AP_ARG   := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin 
 
 # Vold
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
-BOARD_VOLD_MAX_PARTITIONS := 28
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
