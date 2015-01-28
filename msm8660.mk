6cb6a4da1a759fec7864091dc1ae5b460684810f
Dan Cartier <nospamdan11@gmail.com>
2015-01-27 19:51:37 -0500

    msm8660: Add qcom aac encoder.
    Change-Id: I04ff3ec4b5eea36ed96fa2da99bc5335745251dc

--- msm8660.mk
+++ msm8660.mk
@@ -51,6 +51,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=131072
 
 PRODUCT_PROPERTY_OVERRIDES += \
+    qcom.hw.aac.encoder=true \
+    ro.config.vc_call_vol_steps=7
+
+PRODUCT_PROPERTY_OVERRIDES += \
     lpa.decode=false \
     persist.sys.media.use-awesome=true \
     camera2.portability.force_api=1
