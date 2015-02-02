cca4bfaab463f522558451cece7df190e5142746
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2015-02-02 14:13:12 -0500

    msm8660: We do not need the script building this twice!
    Conflicts:
    	msm8660.mk
    
    Change-Id: I5f1e15a2eafa364ba412700f7e4ecf200a5995fc

--- msm8660.mk
+++ msm8660.mk
@@ -85,8 +85,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.usb.default \
-    audio_policy.conf \
-    audio.r_submix.default \
+    audio_policy.msm8660 \
     audio.primary.msm8660 \
     libaudio-resampler \
     libaudioutils
