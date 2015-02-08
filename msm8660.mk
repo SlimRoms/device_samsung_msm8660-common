bac39264665c11919d8ec41a75203715393b1bc0
Dan Cartier <nospamdan11@gmail.com>
2015-02-08 17:19:36 -0500

    msm8660: Update audio policy.
    Policy is now coming in from audio-caf/legacy/msm8660.
    
    Change-Id: I93572d5b789d40c2b4acf8ac399fbe9464c6a4a2

--- msm8660.mk
+++ msm8660.mk
@@ -85,15 +85,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.usb.default \
+    audio_policy.conf \
+    audio.r_submix.default \
     audio_policy.msm8660 \
     audio.primary.msm8660 \
     libaudio-resampler \
     libaudioamp \
     libaudioutils
-
-# Audio config
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
 
 # Camera
 PRODUCT_PACKAGES += \
