286ed6c7e9c832dd797441050a2d4a63d2eff35f
Dan Cartier <nospamdan11@gmail.com>
2015-02-21 15:25:23 +0100

    Revert "msm8660: Remove Power HAL."
    This reverts commit 59558e9571ddad8043f8769b1f3f323157c18933.
    
    Change-Id: I1bd35fe59cabd536eb295d9c05ff830b16f41b63

--- msm8660.mk
+++ msm8660.mk
@@ -41,10 +41,7 @@ PRODUCT_COPY_FILES += \
 
 # System properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    wifi.interface=wlan0
-
-
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    wifi.interface=wlan0 \
     persist.sys.usb.config=mtp
 
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -184,6 +181,10 @@ PRODUCT_BOOT_JARS += \
 # Package generation
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/test_radio_version.sh:/system/etc/test_radio_version.sh
+
+# Power HAL
+PRODUCT_PACKAGES += \
+    power.msm8660
 
 # Torch
 PRODUCT_PACKAGES += \
