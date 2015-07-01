f0ecb1be52d9202efd2cf994be065831defc81ee
bryan2894 <bryan2894@gmail.com>
2015-07-01 19:32:29 -0400

    msm8660: create modem symlinks during build
    Conflicts:
    	rootdir/Android.mk
    	rootdir/etc/fstab.qcom
    
    Conflicts:
    	rootdir/etc/fstab.qcom
    	rootdir/etc/init.qcom.rc
    
    Change-Id: If2d37bd60eea7a9a55508d981c4c7413a134eefa

--- Android.mk
+++ Android.mk
@@ -3,5 +3,46 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(BOARD_VENDOR),samsung)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+FIRMWARE_MODEM_IMAGES := \
+    modem.b00 modem.b01 modem.b02 modem.b03 modem.b04 modem.b05 \
+    modem.b06 modem.b07 modem.b08 modem.b09 modem.b10 modem.mdt
+
+FIRMWARE_MODEM_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_MODEM_IMAGES)))
+
+$(FIRMWARE_MODEM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Modem Firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MODEM_SYMLINKS)
+
+FIRMWARE_Q6_IMAGES := \
+    q6.b00 q6.b01 q6.b02 q6.b03 q6.b04 q6.b05 q6.b06 q6.b07 q6.mdt
+
+FIRMWARE_Q6_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_Q6_IMAGES)))
+
+$(FIRMWARE_Q6_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Q6 Firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_Q6_SYMLINKS)
+
+FIRMWARE_MDM_IMAGES := \
+    amss.mbn dbl.mbn dsp1.mbn dsp2.mbn efs1.mbn efs2.mbn efs3.mbn osbl.mbn
+
+FIRMWARE_MDM_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_MDM_IMAGES)))
+
+$(FIRMWARE_MDM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "MDM Firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware-mdm/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MDM_SYMLINKS)
+
 endif
 endif
