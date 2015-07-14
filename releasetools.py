LineageOS/android_device_samsung_galaxys2-common

releasetools: resize system partition after flashing
resize2fs.static is placed in install/bin, possible to run without /system mounted.
running it will resize /system to the actual system partition size,
supporting custom pits with block-based OTAs

Change-Id: Id2c82add1f697ae3d7e5a1031f6a6d51a4ee53e0

releasetools/extensions/releasetools.py
@@ -1,0 +1,10 @@
+import common
+import struct
+
+def FullOTA_PostValidate(info):
+	# run e2fsck
+	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/mmcblk0p9");');
+	# resize2fs: run and delete
+	info.script.AppendExtra('run_program("/tmp/install/bin/resize2fs_static", "/dev/block/mmcblk0p9");');
+	# run e2fsck
+	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/mmcblk0p9");');

SlimRoms/device_samsung_msm8660-common

releasetools.py
@@ -1,0 +1,10 @@
+import common
+import struct
+
+def FullOTA_PostValidate(info):
+	# run e2fsck
+	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/mmcblk0p24");');
+	# resize2fs: run and delete
+	info.script.AppendExtra('run_program("/tmp/install/bin/resize2fs_static", "/dev/block/mmcblk0p24");');
+	# run e2fsck
+	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/mmcblk0p24");');