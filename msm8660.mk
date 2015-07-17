97a9f8eeb3643342017c1f2b4083d8ca8933b939
Thierry Strudel <tstrudel@google.com>
2015-07-16 21:30:00 -0400

    msm8660: force dex2oat not to use swap file
    Change-Id: Ie7c11547da6a56b7d638a5ec2df275e712078fc8

--- msm8660.mk
+++ msm8660.mk
@@ -77,6 +77,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # ART
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-flags=--no-watch-dog \
+    dalvik.vm.dex2oat-swap=false \
     ro.sys.fw.dex2oat_thread_count=4
 
 # Ramdisk
