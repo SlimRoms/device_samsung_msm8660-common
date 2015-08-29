48132865645eb6461863b14ffa9a242f9b311dfe
NoSpamDan <nospamdan1@gmail.com>
2015-08-29 20:26:07 +0200

    msm8660: Address a few sepolicy denials
    Fix mobile data, mpdecision not working with SELinux set to Enforcing.
    
    Credit goes to Anas Karbila (anaskarbila@gmail.com) for helping with this.
    
    Change-Id: I1565cc485a6919f6afc1d383b96e7f8ac1b06371

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -102,7 +102,6 @@ BOARD_SEPOLICY_UNION += \
     bluetooth.te \
     bootanim.te \
     device.te \
-    domain.te \
     drmserver.te \
     file.te \
     file_contexts \
@@ -124,6 +123,7 @@ BOARD_SEPOLICY_UNION += \
     power_dev.te \
     property_contexts \
     qmuxd.te \
+    radio.te \
     recovery.te \
     rild.te \
     rmt_storage.te \
