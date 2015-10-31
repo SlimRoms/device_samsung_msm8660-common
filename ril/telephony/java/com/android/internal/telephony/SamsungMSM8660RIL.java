157d64823a00ed6752ca767f7de869f5a509df99
Mattia D'Alleva <antaresone@antaresone.eu>
2015-10-31 16:11:16 +0100

    ril: update constructors for M
    Change-Id: I1b1fa3c1ee71e785752d80b9f6ead1266eb7e4b1

--- ril/telephony/java/com/android/internal/telephony/SamsungMSM8660RIL.java
+++ ril/telephony/java/com/android/internal/telephony/SamsungMSM8660RIL.java
@@ -67,16 +67,15 @@ public class SamsungMSM8660RIL extends RIL implements CommandsInterface {
     public static final long SEND_SMS_TIMEOUT_IN_MS = 30000;
     private boolean samsungEmergency = needsOldRilFeature("samsungEMSReq");
 
-    public SamsungMSM8660RIL(Context context, int preferredNetworkType,
-            int cdmaSubscription, Integer instanceId) {
-        super(context, preferredNetworkType, cdmaSubscription, instanceId);
+    public SamsungMSM8660RIL(Context context, int networkModes, int cdmaSubscription) {
+        this(context, networkModes, cdmaSubscription, null);
         mAudioManager = (AudioManager)mContext.getSystemService(Context.AUDIO_SERVICE);
         mQANElements = SystemProperties.getInt("ro.ril.telephony.mqanelements", 4);
     }
 
-    public SamsungMSM8660RIL(Context context, int networkMode,
-            int cdmaSubscription) {
-        super(context, networkMode, cdmaSubscription, null);
+    public SamsungMSM8660RIL(Context context, int preferredNetworkType,
+            int cdmaSubscription, Integer instanceId) {
+        super(context, preferredNetworkType, cdmaSubscription, instanceId);
         mAudioManager = (AudioManager)mContext.getSystemService(Context.AUDIO_SERVICE);
         mQANElements = SystemProperties.getInt("ro.ril.telephony.mqanelements", 4);
     }
