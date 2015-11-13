e2e9f7638a75226f0b576fa2d98ac6b1b45277bf
bryan2894 <bryan2894@gmail.com>
2015-11-12 22:56:42 -0500

    msm8660: update script for virtual partitioning
    Change-Id: I6df4834ea5031e0cdb08ca5cc0c8dc2a5c6bf01d

--- releasetools.py
+++ releasetools.py
@@ -3,8 +3,8 @@ import struct
 
 def FullOTA_PostValidate(info):
 	# run e2fsck
-	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/mmcblk0p24");');
+	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/mmcblk0p25");');
 	# resize2fs: run and delete
-	info.script.AppendExtra('run_program("/tmp/install/bin/resize2fs_static", "/dev/block/mmcblk0p24");');
+	info.script.AppendExtra('run_program("/tmp/install/bin/resize2fs_static", "/dev/block/mmcblk0p25");');
 	# run e2fsck
-	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/mmcblk0p24");');
+	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/mmcblk0p25");');
