2a25be49906a75e37071f068049de9b9e3fd63c2
Dan Cartier <nospamdan1@gmail.com>
2015-11-23 14:33:26 -0500

    msm8660-common: Additional default properties for adb/logging
    Conflicts:
    	msm8660.mk
    
    Change-Id: If3768314e530db2d6ef2d10a0b3772fbea4b033f

--- msm8660.mk
+++ msm8660.mk
@@ -108,6 +108,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.enable.chromecast.mirror=true
 
+# Compatibility symbols wrappers
+PRODUCT_PACKAGES += \
+    libboringssl-compat \
+    libsamsung_symbols
+
+# Debug - For userdebug builds
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.secure=0 \
+    ro.adb.secure=0 \
+    ro.debuggable=1 \
+    persist.service.adb.enable=1
+
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8660 \
