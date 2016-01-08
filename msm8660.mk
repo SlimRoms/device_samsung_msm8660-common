a830d06638548bf7cee09150d35e45b152c0006b
bryan2894 <bryan2894@gmail.com>
2016-01-08 10:37:09 -0500

    msm8660: bring back DeviceSettings app again

--- msm8660.mk
+++ msm8660.mk
@@ -158,6 +158,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
 
+# MSM8660Settings
+PRODUCT_PACKAGES += \
+    MSM8660Settings
+
 # NFC
 PRODUCT_PACKAGES += \
     libnfc \
