77cf76920878205733f1b3d531e6e463a02a4ea2
Dan Cartier <nospamdan1@gmail.com>
2016-04-07 02:21:47 -0400

    msm8660: Fix recovery path in makefile
    Change-Id: I2f7574e126500bfee6842fbce9b4275aae13905a

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -1,8 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE       := twrp.fstab
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := recovery/twrp.fstab
+LOCAL_SRC_FILES    := twrp.fstab
 LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)/etc
 include $(BUILD_PREBUILT)
 
@@ -10,6 +12,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := postrecoveryboot.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := recovery/postrecoveryboot.sh
+LOCAL_SRC_FILES    := postrecoveryboot.sh
 LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 include $(BUILD_PREBUILT)
