c225534d8752e62b8e1515a2bbe202b3357a5b0b
bryan2894 <bryan2894@gmail.com>
2016-04-08 20:26:16 -0400

    releasetools: update partition check script
    * Thanks to easyaspi314 for the ideas
    
    Change-Id: I459c9568aa1d5afee72f95f8e4b406a8a71837f7

--- releasetools/partitioncheck.sh
+++ releasetools/partitioncheck.sh
@@ -8,17 +8,15 @@
 #   result 1: device is not virtually partitioned
 
 # Variables
-# Size in KiB
-DATA_SIZE=$(df -k /data | tail -n 1 | awk '{ print $1 }');
-SYSTEM_SIZE=$(df -k /system | tail -n 1 | awk '{ print $1 }');
-GB_11=11534336 # 11GB
-GB_1_5=1572864 # 1.5GB
-GB_2=2066172 # 2GB
 # Partitions
-DATA_PARTITION=$(df -k /data | tail | grep /dev | awk '{ print $1 }');
-SYSTEM_PARTITION=$(df -k /system | tail | grep /dev | awk '{ print $1 }');
+DATA_FORMAT=$(mount | grep /data | awk '{ print $5 }');
+DATA_PARTITION=$(mount | grep /data | awk '{ print $1 }');
+SYSTEM_PARTITION=$(mount | grep /system | awk '{ print $1 }');
 VP_DATA_PARTITION="/dev/block/mmcblk0p28"
 VP_SYSTEM_PARTITION="/dev/block/mmcblk0p25"
+# PIT
+STOCK_PIT_MD5="b8997d010456d4f1986db968b28c53ec"
+PIT_MD5=$(md5sum /dev/block/mmcblk0p11 | awk '{ print $1 }');
 
 # ui_print by Chainfire
 OUTFD=$(\
@@ -48,28 +46,30 @@ ui_print() {
 
 # Partition Check
 ui_print "Checking partitions..."
+
 if [ "$DATA_PARTITION" != "$VP_DATA_PARTITION" ]; then
   if [ "$SYSTEM_PARTITION" != "$VP_SYSTEM_PARTITION" ]; then
     ui_print "Partition layout invalid for build!"
-    ui_print "You are most likely using a non-virtually partitioned recovery"
+    ui_print "You are using a non-virtually partitioned recovery"
     ui_print "Make sure you followed the instructions for virtually partitioning your device"
     ui_print "Visit the XDA thread for your device for more information"
     exit 1
   fi
 fi
 
-if [ "$DATA_SIZE" -lt "$GB_11" ]; then
-  if [ "$SYSTEM_SIZE" -lt "$GB_1_5" ]; then
-    ui_print "/data and /system size are too small. Did you repartition?"
-    ui_print "Make sure you followed the instructions for virtually partitioning your device"
-    ui_print "Visit the XDA thread for your device for more information"
-    exit 1
-  else
-    ui_print "/data size is too small. Did you repartition or flash a custom PIT?"
-    ui_print "Make sure to flash back to stock PIT layout, then virtually partition your device"
-    ui_print "Visit the XDA thread for your device for more information"
-    exit 1
-  fi
+if [ "$PIT_MD5" != "$STOCK_PIT_MD5" ]; then
+  ui_print "Invalid PIT detected!"
+  ui_print "You'll need to flash back to stock PIT"
+  ui_print "Visit the XDA thread for your device for more information"
+  exit 1
+fi
+
+if [ "$DATA_FORMAT" == "vfat" ]; then
+  ui_print "Invalid format for data!"
+  ui_print "Data should be formatted as ext4 or f2fs"
+  ui_print "Make sure you followed the instructions for virtually partitioning your device"
+  ui_print "Visit the XDA thread for your device for more information"
+  exit 1
 fi
 
 # Good 2 Go
